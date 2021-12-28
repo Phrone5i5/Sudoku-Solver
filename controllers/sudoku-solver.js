b9a1108fe9dcb80073945c93d4356c192f8b3d09
AremTheEnigma <xaremx@gmail.com>
2021-12-28 12:12:59 -0600

    Added region checking function

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -12,6 +12,9 @@ class SudokuSolver {
 
     }
   }
+  /*
+    Could row, column, and region checking be optimized by stopping once row/column/region containing the coordinate to input the value has been completed?
+  */
   checkRowPlacement(puzzleString, row, column, value) {
     column = column--; // make column allign with its on board space
     let rows = {
@@ -37,9 +40,10 @@ class SudokuSolver {
   }
 
   checkColPlacement(puzzleString, row, column, value) {
-    const rowNumber = (rowLetter) => {
-      const rowPairs = [ ['A', 0], ['B', 1], ['C', 2], ['D', 3], ['E', 4], ['F', 5], ['G', 6], ['H', 7], ['I', 8] ];
-      return rowPairs.filter(pair => pair[0] == rowLetter)[0][1];
+    // Function to convert input row letter to number for string indexing
+    const getRowNumber = (rowLetter) => {
+      return [ ['A', 0], ['B', 1], ['C', 2], ['D', 3], ['E', 4], ['F', 5], ['G', 6], ['H', 7], ['I', 8] ]
+             .filter(pair => pair[0] == rowLetter)[0][1];
     }
     let columns = { 1: '', 2: '', 3: '', 4: '', 5: '', 6: '', 7: '', 8: '', 9: '' };
     // Populating the columns object with the current values of each column
@@ -51,21 +55,67 @@ class SudokuSolver {
       }
       columns[outerIndex + 1] = currentColumn;
     });
-    return columns[column][rowNumber(row)] == '.' && !columns[column].includes(value) ? true : false;
+    return columns[column][getRowNumber(row)] == '.' && !columns[column].includes(value) ? true : false;
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
-    let regions = {
-      topLeft: '',
-      topCenter: '',
-      topRight: '',
-      middleLeft: '',
-      middleCenter: '',
-      middleRight: '',
-      bottomLeft: '',
-      bottomCenter: '',
-      bottomRight: ''
+    // A modified of the previous getRowNumber function, specifcally for returning an index of three rows
+    const getRowNumber = (rowLetter) => {
+      return [ ['A', 0], ['B', 1], ['C', 2], ['D', 0], ['E', 1], ['F', 2], ['G', 0], ['H', 1], ['I', 2] ]
+             .filter(pair => pair[0] == rowLetter)[0][1];
+    }
+    const regions = {
+      topLeft: { regionArr: [], rows: ['A', 'B', 'C'], columns: [1, 2, 3] },
+      topCenter: { regionArr: [], rows: ['A', 'B', 'C'], columns: [4, 5, 6] },
+      topRight: { regionArr: [], rows: ['A', 'B', 'C'], columns: [7, 8, 9] },
+      middleLeft: { regionArr: [], rows: ['D', 'E', 'F'], columns: [1, 2, 3] },
+      middleCenter: { regionArr: [], rows: ['D', 'E', 'F'], columns: [4, 5, 6] },
+      middleRight: { regionArr: [], rows: ['D', 'E', 'F'], columns: [7, 8, 9] },
+      bottomLeft: { regionArr: [], rows: ['G', 'H', 'I'], columns: [1, 2, 3] },
+      bottomCenter: { regionArr: [], rows: ['G', 'H', 'I'], columns: [4, 5, 6] },
+      bottomRight: { regionArr: [], rows: ['G', 'H', 'I'], columns: [7, 8, 9] }
     };
+    regionKeys = Object.keys(regions);
+    regionKeys.forEach((key, current) => {
+    	let currentIndex;
+      switch (current) {
+      	case 0 : currentIndex = 0;
+          break;
+       	case 1 : currentIndex = 3;
+          break;
+        case 2 : currentIndex = 6;
+        	break;
+        case 3 : currentIndex = 27;
+        	break;
+        case 4 : currentIndex = 30;
+        	break;
+        case 5 : currentIndex = 33;
+        	break;
+        case 6 : currentIndex = 54;
+        	break;
+        case 7 : currentIndex = 57;
+        	break;
+        case 8 : currentIndex = 60;
+        	break;
+      }
+      regions[key].regionArr.push(
+      	puzzleString.slice(currentIndex, currentIndex + 3), // current region top row
+        puzzleString.slice(currentIndex + 9, currentIndex + 12), // current region middle row
+        puzzleString.slice(currentIndex + 18, currentIndex + 21) // current region bottom row
+      );
+    });
+    let result = false;
+    regionKeys.forEach(key => {
+    	if (regions[key].rows.includes(row) && regions[key].columns.includes(column)) {
+      	// TODO
+        // check to see if the regionArr row that corresponds to the entered row has a '.' at the column value index,
+        // and that no other space in the regionArr contains the input value 
+        if (regions[key].regionArr[getRowNumber(row)][column - 1] == '.') {
+          result = true;
+        }      	
+      }
+    });
+    return result;
   }
 
   solve(puzzleString) {
