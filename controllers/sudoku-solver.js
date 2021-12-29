55ed9a1d7d1db5f26798d376b288c575efccfd9c
AremTheEnigma <xaremx@gmail.com>
2021-12-29 16:27:33 -0600

    added starter skeleton for solve()

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -4,17 +4,19 @@ class SudokuSolver {
     const areAllValid = (string) => {
       return string.match(/([1-9]|\.)/g).length == 81 ? true : false;
     }
-    if (typeof puzzleString == 'string' && puzzleString.length == 81 && areAllValid(puzzleString)) {
+    if (!puzzleString) {
+      return { valid: false, error: 'Required field missing' };
+    } else if (typeof puzzleString == 'string' && puzzleString.length == 81 && areAllValid(puzzleString)) {
       return { valid: true };
     } else if (typeof puzzleString == 'string' && puzzleString.length == 81 && !areAllValid(puzzleString)) {
       return { valid: false, error: 'Invalid characters in puzzle' };
-    } else {
+    } else if (puzzleString.length != 81) {
       return { valid: false, error: 'Expected puzzle to be 81 characters long' };
+    } else {
+      return { valid: false, error: 'Puzzle cannot be solved'};
     }
   }
-  /*
-    Could row, column, and region checking be optimized by stopping once row/column/region containing the coordinate to input the value has been completed?
-  */
+
   checkRowPlacement(puzzleString, row, column, value) {
     let columnIndex = column - 1; // make column allign with its on board space
     let rows = {
@@ -29,11 +31,9 @@ class SudokuSolver {
       I: ''
     };
     // Populating the rows object with the current values of each row
-    let startIndex = 0;
     let rowKeys = Object.keys(rows);
-    rowKeys.forEach((key) => {
-      rows[key] = puzzleString.slice(startIndex * 9, (startIndex * 9) + 9);
-      startIndex++;
+    rowKeys.forEach((key, current) => {
+      rows[key] = puzzleString.slice(current * 9, (current * 9) + 9);
     });
     // Test the validity of the row, column, and value inputs against the calculated rows
     return rows[row][columnIndex] == '.' && !rows[row].includes(value) ? true : false;
@@ -122,7 +122,66 @@ class SudokuSolver {
   }
 
   solve(puzzleString) {
-    
+    // If initial puzzleString is invalid, exit immediately with error message
+    if (this.validate(puzzleString).valid == false) {
+      return this.validate(puzzleString);
+    }
+    // Declare a function that we will call recursively inside of solve until we have a complete and valid puzzleString
+    const recursiveSolve = (currentPuzzleString) => {
+      // If all columns and rows have been eliminated, return the current string, allowing the stack to return the final string
+      if (!currentPuzzleString.length) {
+        return currentPuzzleString;
+      } else {
+        // A function to convert a row number to a letter for checkRow, checkCol, and checkRegion tests
+        // *** Letters may need to convert to numbers starting with zero, depending on where rowNumber is being input from ***
+        const getRowLetterForChecks = (rowNumber) => {
+          return [ ['A', 1], ['B', 2], ['C', 3], ['D', 4], ['E', 5], ['F', 6], ['G', 7], ['H', 8], ['I', 9] ]
+                 .filter(pair => pair[1] == rowNumber)[0][0];
+        }
+        // Setting up an object containing all rows
+        let rows = {
+          A: '',
+          B: '',
+          C: '',
+          D: '',
+          E: '',
+          F: '',
+          G: '',
+          H: '',
+          I: ''
+        };
+        let rowKeys = Object.keys(rows);
+        rowKeys.forEach((key, current) => {
+          rows[key] = currentPuzzleString.slice(current * 9, (current * 9) + 9);
+        });
+        // TODO
+        // Iterate through columns using a counter and checkColPlacement() until one is found with a blank space (c)
+        // Iterate* through the rows using a counter and checkRowPlacement() until one is found with a blank space (r) (should be the same blank as found by checkColPlacement())
+            // *1 both iterators should most likely be while loops with dedicated values to break the loop once the values have been set (next step)
+        // Use getRowLetterForChecks with the current index to get the correct row letter, and then change that row's value* at the index of the checkColPlacement() iterator
+            // *2 This value should be set using another iterator, where if it passes checkColPlacement(), checkRowPlacement(), and checkRegionPlacement(), the value is 1, if not, 2, etc,
+            // this may require a rewrite of the functions to either only make rows/columns/regions from however many rows/columns worth of characters are available, or find a way...
+            // ...to preserve the original input string, making a temp copy, adding the prospective value at the correct index, and then checking it with all three placement checking...
+            // ... functions. 
+        // Once the value has been set, we need to remove rows and columns where all values are filled.
+        /* For each column (j) (filter columns?) where r[j] = non blank value {
+            For each row (i) where i[j] = non blank value {
+              delete row i from overall current object
+            }
+           }
+           delete column j from overall current object
+        */
+        
+        // change newInputString to the new string minus the rows/columns/whatever here
+        let newInputString = '';
+
+        // the function returns itself with the new puzzle string, bypassing the validate for the entire function,
+        // taking us back to line 132...
+        return recursiveSolve(newPuzzleString);
+      }
+    }
+    // after recursive function setup, call the function, kicking off the solving process.
+    return { solution: recursiveSolve(puzzleString) };
   }
 }
 
